573bb38211d1f16a1cfd0fe0505fa38a11364818
Iliaromanov <iliaromanov033@gmail.com>
2021-11-29 16:06:41 -0500

    DefaultComputer player class header

---
+++ DefaultComputer.h
@@ -0,0 +1,11 @@
+#ifndef DEFAULTCOMP_H
+#define DEFAULTCOMP_H
+#include "Player.h"
+
+class DefaultComputer : public Player { // ConcreteElementB
+    public:
+        // makeMove returns d for print deck, r for ragequit, and 0 otherwise
+        char makeMove(Game &game) override;
+};
+
+#endif
