0e2ce8eed59fb2540d685e34d6a11b734e7bc1dc
Iliaromanov <iliaromanov033@gmail.com>
2021-11-29 16:04:41 -0500

    card deck class header

---
+++ Deck.h
@@ -0,0 +1,34 @@
+#ifndef DECK_H
+#define DECK_H
+#include <string>
+#include <memory>
+#include <vector>
+#include <random>
+
+class Player;
+
+class Deck {
+    class Card;
+
+    std::vector<std::unique_ptr<Card>> cards; // 52 cards
+    std::default_random_engine rng; // for shuffling
+
+    class Card {
+        unsigned int value; // 1 to 13
+        char suite; // H, D, C, or S
+        std::string name; // eg. AS or 7H or KD etc.
+
+        public: 
+            Card(int value, char suite, std::string name); // ctor
+    };
+
+    public:
+        void shuffle(); // shuffles cards in deck using rng
+        void dealCards(Player *p1, Player *p2, Player *p3, Player *p4); // populates players hand vec
+        void printDeck(); // prints cards in the cards vector
+        Deck(std::default_random_engine rng); // ctor
+        ~Deck(); // dtor
+};
+
+
+#endif
