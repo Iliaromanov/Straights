57ba945ed3483959a539a413a2e2cbb67885e6e6
Iliaromanov <iliaromanov033@gmail.com>
2021-11-29 16:05:44 -0500

    game abstract base class header

---
+++ Game.h
@@ -0,0 +1,23 @@
+#ifndef GAME_H
+#define GAME_H
+
+class Human;
+class DefaultComputer;
+
+class Game { // Visitor abstract base class
+    // current turn #; 0 to 3
+    // turnNum=n means its Player<n+1>s turn
+    unsigned int turnNum;
+
+    public:
+        void nextTurn(); // updates turnNum
+        int getTurnNum(); // getter
+        void virtual visit(Human *human) = 0;
+        void virtual visit(DefaultComputer *comp) = 0;
+
+        Game(unsigned int turnNum); // ctor
+        ~Game(); // dtor
+};
+
+
+#endif
